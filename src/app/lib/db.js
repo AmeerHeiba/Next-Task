ab898766ef0d5f696530a5b84ca39bd696e35a12
AmeerHeiba <31767185+AmeerHeiba@users.noreply.github.com>
2024-09-24 02:00:44 +0300

    updating mongo to use ssl for production

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -3,7 +3,10 @@ import { MongoClient } from "mongodb";
 let db;
 if (!db) {
   const mongoClient = new MongoClient("mongodb+srv://ameerabdullahheiba:rV7kiXJlGh6rlGKz@cluster0.8i1jk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-  const clientPromise = await mongoClient.connect();
+  const options = {
+    ssl:true
+  }
+  const clientPromise = await mongoClient.connect(options);
   db = clientPromise.db();
 }
 

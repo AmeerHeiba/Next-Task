383cfc105ab9d64780c413f61fbd205edba7c62a
AmeerHeiba <31767185+AmeerHeiba@users.noreply.github.com>
2024-09-24 02:34:26 +0300

    db options

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -7,6 +7,7 @@ if (!db) {
   const mongoClient = new MongoClient(uri, {
     ssl: true,
     useUnifiedTopology: true,
+    useNewUrlParser: true,
   });
   const clientPromise = await mongoClient.connect();
   db = clientPromise.db();
@@ -16,4 +17,4 @@ export default db;
 
 
 
-//mongodb+srv://ameerabdullahheiba:rV7kiXJlGh6rlGKz@cluster0.8i1jk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+//mongodb+srv://ameerabdullahheiba:rV7kiXJlGh6rlGKz@cluster0.8i1jk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/next?retryWrites=true&w=majority

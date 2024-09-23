6753f449e6dd349f2e8d62748d870e99ad3f01d0
AmeerHeiba <31767185+AmeerHeiba@users.noreply.github.com>
2024-09-24 02:42:56 +0300

    ++

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -4,17 +4,21 @@ let db;
 const uri = process.env.MONGODB_URI;
 
 if (!db) {
-  const mongoClient = new MongoClient(uri, {
-    ssl: true,
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  });
-  const clientPromise = await mongoClient.connect();
-  db = clientPromise.db();
+  try {
+    const mongoClient = new MongoClient(uri, {
+      ssl: true,  
+    });
+    
+    // Connect and set db
+    const client = await mongoClient.connect();
+    db = client.db(); 
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw new Error("Failed to connect to MongoDB");
+  }
 }
 
 export default db;
-
 
 
 //mongodb+srv://ameerabdullahheiba:rV7kiXJlGh6rlGKz@cluster0.8i1jk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/next?retryWrites=true&w=majority
